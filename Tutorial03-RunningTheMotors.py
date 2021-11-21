b2ca5817e15ee51d694f77818059a665d086c05f
Jim (Dimitrios) Andrakakis <dandraka@gmail.com>
2021-11-21 16:42:39 +0100

    Update Tutorial03-RunningTheMotors.py

--- Tutorial03-RunningTheMotors.py
+++ Tutorial03-RunningTheMotors.py
@@ -3,8 +3,10 @@
 import time # Import the Time library
 from gpiozero import CamJamKitRobot # Import the CamJam GPIO Zero Library
 robot = CamJamKitRobot()
+time.sleep(5)
 # Turn the motors on
 robot.forward()
 # Wait for 1 seconds
-time.sleep(1)
-# Turn the mo
+time.sleep(2)
+# Turn the motors off
+robot.stop()
