7b2b439e37ab12f6a4954e2996aca75aab8a424f
Jim (Dimitrios) Andrakakis <dandraka@gmail.com>
2021-11-21 17:02:50 +0100

    Update 5-line.py

--- 5-line.py
+++ 5-line.py
@@ -8,17 +8,17 @@ sensor = LineSensor(pinLineFollower)
 # Define the functions that will be called when the line is
 # detected or not detected
 def lineseen():
-print("Line seen")
+  print("Line seen")
 def linenotseen():
-print("No line seen")
+  print("No line seen")
 # Tell the program what to do with a line is seen
 sensor.when_line = lineseen
 # And when no line is seen
 sensor.when_no_line = linenotseen
 try:
-# Repeat the next indented block forever
-while True:
-time.sleep(10)
+  # Repeat the next indented block forever
+  while True:
+    time.sleep(10)
 # If you press CTRL+C, cleanup and stop
 except KeyboardInterrupt:
-print("Exiting")
+  print("Exiting")
